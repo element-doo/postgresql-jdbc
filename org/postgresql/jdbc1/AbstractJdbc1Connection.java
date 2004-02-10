e596b46dee7f2ff0ee8116c24a9c3f26823223f3
jurka <jurka>
2004-02-10 01:56:46 +0000

    Don't try to turn the random bytes from a md5 salt into a String because it may not be a valid String depending on the encoding.
    Per report from Nadeem Bitar.

--- org/postgresql/jdbc1/AbstractJdbc1Connection.java
+++ org/postgresql/jdbc1/AbstractJdbc1Connection.java
@@ -9,7 +9,7 @@
  * Copyright (c) 2003, PostgreSQL Global Development Group
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgjdbc/org/postgresql/jdbc1/AbstractJdbc1Connection.java,v 1.30 2004/01/13 07:19:29 jurka Exp $
+ *	  $PostgreSQL: pgjdbc/org/postgresql/jdbc1/AbstractJdbc1Connection.java,v 1.31 2004/02/10 01:56:46 jurka Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -349,9 +349,13 @@ public abstract class AbstractJdbc1Connection implements BaseConnection
 							md5Salt[1] = (byte)pgStream.ReceiveChar();
 							md5Salt[2] = (byte)pgStream.ReceiveChar();
 							md5Salt[3] = (byte)pgStream.ReceiveChar();
-							salt = new String(md5Salt, 0, 4);
-							if (Driver.logDebug)
-								Driver.debug("MD5 salt=" + salt);
+							if (Driver.logDebug) {
+								String md5SaltString = "";
+								for (int i=0; i<md5Salt.length; i++) {
+									md5SaltString += " " + md5Salt[i];
+								}
+								Driver.debug("MD5 salt=" + md5SaltString);
+							}
 						}
 
 						// now send the auth packet
@@ -631,9 +635,13 @@ public abstract class AbstractJdbc1Connection implements BaseConnection
 							md5Salt[1] = (byte)pgStream.ReceiveChar();
 							md5Salt[2] = (byte)pgStream.ReceiveChar();
 							md5Salt[3] = (byte)pgStream.ReceiveChar();
-							salt = new String(md5Salt, 0, 4);
-							if (Driver.logDebug)
-								Driver.debug("MD5 salt=" + salt);
+							if (Driver.logDebug) {
+								String md5SaltString = "";
+								for (int i=0; i<md5Salt.length; i++) {
+									md5SaltString += " " + md5Salt[i];
+								}
+								Driver.debug("MD5 salt=" + md5SaltString);
+							}
 						}
 
 						// now send the auth packet
