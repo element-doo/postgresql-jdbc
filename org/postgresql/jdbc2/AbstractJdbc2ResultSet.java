bfc2bcbe4449e4a29300484d2070f24309596e3f
jurka <jurka>
2004-10-14 08:51:07 +0000

    Further optimization of ResultSet.findColumn.  Constuct the HashMap cache on demand and initially seed it with the column names.
    Oliver Jowett

--- org/postgresql/jdbc2/AbstractJdbc2ResultSet.java
+++ org/postgresql/jdbc2/AbstractJdbc2ResultSet.java
@@ -8,7 +8,7 @@
  * Copyright (c) 2003, PostgreSQL Global Development Group
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgjdbc/org/postgresql/jdbc2/AbstractJdbc2ResultSet.java,v 1.51 2004/10/13 20:58:01 jurka Exp $
+ *	  $PostgreSQL: pgjdbc/org/postgresql/jdbc2/AbstractJdbc2ResultSet.java,v 1.52 2004/10/14 08:51:07 jurka Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -79,7 +79,7 @@ public abstract class AbstractJdbc2ResultSet implements BaseResultSet, org.postg
 	protected int fetchSize;       // Current fetch size (might be 0).
 	protected ResultCursor cursor; // Cursor for fetching additional data.
 
-	private HashMap columnNameIndexMap = new HashMap(); // Speed up findColumn by caching lookups
+	private HashMap columnNameIndexMap; // Speed up findColumn by caching lookups
 
 	public abstract ResultSetMetaData getMetaData() throws SQLException;
 
@@ -2246,18 +2246,22 @@ public abstract class AbstractJdbc2ResultSet implements BaseResultSet, org.postg
 	 */
 	public int findColumn(String columnName) throws SQLException
 	{
+		if (columnNameIndexMap == null) {
+			columnNameIndexMap = new HashMap(fields.length*2);
+			for (int i=0; i<fields.length; i++) {
+				columnNameIndexMap.put(fields[i].getColumnLabel().toLowerCase(), new Integer(i+1));
+			}
+		}
+
 		Integer index = (Integer)columnNameIndexMap.get(columnName);
 		if (index != null) {
 			return index.intValue();
 		}
 
-		final int flen = fields.length;
-		for (int i = 0 ; i < flen; ++i) {
-			if (fields[i].getColumnLabel().equalsIgnoreCase(columnName)) {
-				index = new Integer(i+1);
-				columnNameIndexMap.put(columnName, index);
-				return index.intValue();
-			}
+		index = (Integer)columnNameIndexMap.get(columnName.toLowerCase());
+		if (index != null) {
+			columnNameIndexMap.put(columnName, index);
+			return index.intValue();
 		}
 
 		throw new PSQLException (GT.tr("The column name '{0}' was not found in this ResultSet.", columnName));
