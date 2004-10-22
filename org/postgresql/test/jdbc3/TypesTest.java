4fee52149f48d3d531e74a4ba09d6b435f4668a2
jurka <jurka>
2004-10-22 21:09:22 +0000

    Try getting $PostgreSQL$ keyword substitution to work.

--- org/postgresql/test/jdbc3/TypesTest.java
+++ org/postgresql/test/jdbc3/TypesTest.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import junit.framework.TestCase;
 import org.postgresql.test.TestUtil;
 
-/* $PostgreSQL $ */
+/* $PostgreSQL: pgjdbc/org/postgresql/test/jdbc3/TypesTest.java,v 1.2 2004/10/22 21:09:22 jurka Exp $ */
 
 public class TypesTest extends TestCase {
 
