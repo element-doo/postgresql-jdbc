7aac2c6f5d6dba263e6129ba26186421b44525a8
jurka <jurka>
2004-11-09 08:42:48 +0000

    Indent source code with: astyle -j -p --convert-tabs

--- org/postgresql/PGNotification.java
+++ org/postgresql/PGNotification.java
@@ -1,12 +1,12 @@
 /*-------------------------------------------------------------------------
- *
- * Copyright (c) 2003-2004, PostgreSQL Global Development Group
- *
- * IDENTIFICATION
- *	  $PostgreSQL: pgjdbc/org/postgresql/PGNotification.java,v 1.9 2004/11/07 22:15:29 jurka Exp $
- *
- *-------------------------------------------------------------------------
- */
+*
+* Copyright (c) 2003-2004, PostgreSQL Global Development Group
+*
+* IDENTIFICATION
+*   $PostgreSQL: pgjdbc/org/postgresql/PGNotification.java,v 1.10 2004/11/09 08:43:38 jurka Exp $
+*
+*-------------------------------------------------------------------------
+*/
 package org.postgresql;
 
 /**
@@ -14,26 +14,26 @@ package org.postgresql;
  */
 public interface PGNotification
 {
-	/**
-	 * Returns name of this notification
-	 * @since 7.3
-	 */
-	public String getName();
+    /**
+     * Returns name of this notification
+     * @since 7.3
+     */
+    public String getName();
 
-	/**
-	 * Returns the process id of the backend process making this notification
-	 * @since 7.3
-	 */
-	public int getPID();
+    /**
+     * Returns the process id of the backend process making this notification
+     * @since 7.3
+     */
+    public int getPID();
 
-	/**
-	 * Returns additional information from the notifying process.
-	 * Currently, this feature is unimplemented and always returns
-	 * an empty String.
-	 *
-	 * @since 8.0
-	 */
-	public String getParameter();
+    /**
+     * Returns additional information from the notifying process.
+     * Currently, this feature is unimplemented and always returns
+     * an empty String.
+     *
+     * @since 8.0
+     */
+    public String getParameter();
 
 }
 
--- org/postgresql/jdbc3/Jdbc3SimpleDataSource.java
+++ org/postgresql/jdbc3/Jdbc3SimpleDataSource.java
@@ -1,12 +1,12 @@
 /*-------------------------------------------------------------------------
- *
- * Copyright (c) 2004, PostgreSQL Global Development Group
- *
- * IDENTIFICATION
- *	  $PostgreSQL: pgjdbc/org/postgresql/jdbc3/Jdbc3SimpleDataSource.java,v 1.3 2004/11/07 22:16:32 jurka Exp $
- *
- *-------------------------------------------------------------------------
- */
+*
+* Copyright (c) 2004, PostgreSQL Global Development Group
+*
+* IDENTIFICATION
+*   $PostgreSQL: pgjdbc/org/postgresql/jdbc3/Jdbc3SimpleDataSource.java,v 1.4 2004/11/09 08:50:38 jurka Exp $
+*
+*-------------------------------------------------------------------------
+*/
 package org.postgresql.jdbc3;
 
 import org.postgresql.ds.PGSimpleDataSource;
