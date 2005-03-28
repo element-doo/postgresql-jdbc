9d1271523b135960c935bc7ff3c5e04b7965219f
jurka <jurka>
2005-03-28 08:52:35 +0000

    Implement JDBC 3 Blob methods to allow writing.
    Michael Barker

--- org/postgresql/test/jdbc3/Jdbc3TestSuite.java
+++ org/postgresql/test/jdbc3/Jdbc3TestSuite.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/test/jdbc3/Jdbc3TestSuite.java,v 1.12 2005/02/01 07:27:55 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/test/jdbc3/Jdbc3TestSuite.java,v 1.13 2005/03/28 08:52:35 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -27,6 +27,7 @@ public class Jdbc3TestSuite extends TestSuite
         suite.addTestSuite(TypesTest.class);
         suite.addTestSuite(ResultSetTest.class);
         suite.addTestSuite(ParameterMetaDataTest.class);
+        suite.addTestSuite(Jdbc3BlobTest.class);
         return suite;
     }
 }
