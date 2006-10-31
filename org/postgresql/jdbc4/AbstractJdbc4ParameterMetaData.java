5f0c09734758125cc3382842af88a4cb07f4ea18
jurka <jurka>
2006-10-31 06:12:46 +0000

    Fixes for latest JDK1.6 snapshot.  They've added a whole bunch of new methods, so stub them out as well.

--- org/postgresql/jdbc4/AbstractJdbc4ParameterMetaData.java
+++ org/postgresql/jdbc4/AbstractJdbc4ParameterMetaData.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/jdbc4/AbstractJdbc4ParameterMetaData.java,v 1.1 2006/06/08 10:34:52 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/jdbc4/AbstractJdbc4ParameterMetaData.java,v 1.2 2006/10/31 06:12:47 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -26,9 +26,9 @@ public abstract class AbstractJdbc4ParameterMetaData extends org.postgresql.jdbc
         throw org.postgresql.Driver.notImplemented(this.getClass(), "isWrapperFor(Class<?>)");
     }
 
-    public Object unwrap(Class<?> iface) throws SQLException
+    public <T> T unwrap(Class<T> iface) throws SQLException
     {
-        throw org.postgresql.Driver.notImplemented(this.getClass(), "unwrap(Class<?>)");
+        throw org.postgresql.Driver.notImplemented(this.getClass(), "unwrap(Class<T>)");
     }
 
 }
