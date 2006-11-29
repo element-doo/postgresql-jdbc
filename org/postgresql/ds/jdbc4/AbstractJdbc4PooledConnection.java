804e78180209df3bbce24ae33ec46f69ccad662f
jurka <jurka>
2006-11-29 04:00:15 +0000

    Allow datasource code to build with JDK1.6 by splitting off jdbcN directories in the fashion of the core driver.  To maintain the generic interfaces like PGPoolingDataSource I had to use ant's conditional compilation to determine which implementation class to extend from.  It's ugly, but contained and works.

---
+++ org/postgresql/ds/jdbc4/AbstractJdbc4PooledConnection.java
@@ -0,0 +1,32 @@
+/*-------------------------------------------------------------------------
+*
+* Copyright (c) 2004-2005, PostgreSQL Global Development Group
+*
+* IDENTIFICATION
+*   $PostgreSQL: pgjdbc/org/postgresql/ds/jdbc4/AbstractJdbc4PooledConnection.java,v 1.1 2006/11/29 04:00:27 jurka Exp $
+*
+*-------------------------------------------------------------------------
+*/
+package org.postgresql.ds.jdbc4;
+
+import java.sql.Connection;
+import javax.sql.StatementEventListener;
+import org.postgresql.ds.jdbc23.AbstractJdbc23PooledConnection;
+
+public abstract class AbstractJdbc4PooledConnection extends AbstractJdbc23PooledConnection
+{
+
+    public AbstractJdbc4PooledConnection(Connection con, boolean autoCommit, boolean isXA)
+    {
+        super(con, autoCommit, isXA);
+    }
+
+    public void removeStatementEventListener(StatementEventListener listener)
+    {
+    }
+
+    public void addStatementEventListener(StatementEventListener listener)
+    {
+    }
+
+}
---
+++ org/postgresql/ds/jdbc4/AbstractJdbc4SimpleDataSource.java
@@ -0,0 +1,28 @@
+/*-------------------------------------------------------------------------
+*
+* Copyright (c) 2004-2005, PostgreSQL Global Development Group
+*
+* IDENTIFICATION
+*   $PostgreSQL: pgjdbc/org/postgresql/ds/jdbc4/AbstractJdbc4SimpleDataSource.java,v 1.1 2006/11/29 04:00:27 jurka Exp $
+*
+*-------------------------------------------------------------------------
+*/
+package org.postgresql.ds.jdbc4;
+
+import java.sql.SQLException;
+
+import org.postgresql.ds.jdbc23.AbstractJdbc23SimpleDataSource;
+
+public class AbstractJdbc4SimpleDataSource extends AbstractJdbc23SimpleDataSource
+{
+    public boolean isWrapperFor(Class<?> iface) throws SQLException
+    {
+        throw org.postgresql.Driver.notImplemented(this.getClass(), "isWrapperFor(Class<?>)");
+    }
+
+    public <T> T unwrap(Class<T> iface) throws SQLException
+    {
+        throw org.postgresql.Driver.notImplemented(this.getClass(), "unwrap(Class<T>)");
+    }
+
+}
