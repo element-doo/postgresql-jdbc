7052d4361d403a0dc7df8a465af9f65b56a6faef
jurka <jurka>
2007-03-29 04:27:52 +0000

    The error message reports the column position incorrectly by using ""+i+1 which is (""+i)+1, resulting in a value of 21 instead of 3 for an i value of 2.

--- org/postgresql/jdbc2/AbstractJdbc2Statement.java
+++ org/postgresql/jdbc2/AbstractJdbc2Statement.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/jdbc2/AbstractJdbc2Statement.java,v 1.98 2007/02/19 06:00:24 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/jdbc2/AbstractJdbc2Statement.java,v 1.99 2007/03/29 04:27:52 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -400,7 +400,7 @@ public abstract class AbstractJdbc2Statement implements BaseStatement
                     else
                     {    
 	                    throw new PSQLException (GT.tr("A CallableStatement function was executed and the out parameter {0} was of type {1} however type {2} was registered.",
-	                            new Object[]{""+i+1,
+	                            new Object[]{new Integer(i+1),
 	                                "java.sql.Types=" + columnType, "java.sql.Types=" + functionReturnType[j] }),
 	                      PSQLState.DATA_TYPE_MISMATCH);
                     }
