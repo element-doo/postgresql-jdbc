24e98bc6238a1e970962ae40d5473ce6d8d6c90f
jurka <jurka>
2007-09-10 08:29:26 +0000

    Don't call new String("xx") because "xx" is already a string and this just makes an extra copy of it.
    Mikko Tiihonen

--- org/postgresql/jdbc2/AbstractJdbc2DatabaseMetaData.java
+++ org/postgresql/jdbc2/AbstractJdbc2DatabaseMetaData.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/jdbc2/AbstractJdbc2DatabaseMetaData.java,v 1.37 2007/07/23 17:30:38 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/jdbc2/AbstractJdbc2DatabaseMetaData.java,v 1.38 2007/09/10 08:29:26 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -2141,7 +2141,7 @@ public abstract class AbstractJdbc2DatabaseMetaData
     {
         Field f[] = new Field[1];
         Vector v = new Vector();
-        f[0] = new Field(new String("TABLE_CAT"), Oid.VARCHAR);
+        f[0] = new Field("TABLE_CAT", Oid.VARCHAR);
         byte[][] tuple = new byte[1][];
         tuple[0] = connection.encodeString(connection.getCatalog());
         v.addElement(tuple);
@@ -2176,7 +2176,7 @@ public abstract class AbstractJdbc2DatabaseMetaData
 
         Field f[] = new Field[1];
         Vector v = new Vector();
-        f[0] = new Field(new String("TABLE_TYPE"), Oid.VARCHAR);
+        f[0] = new Field("TABLE_TYPE", Oid.VARCHAR);
         for (i = 0; i < types.length; i++)
         {
             byte[][] tuple = new byte[1][];
