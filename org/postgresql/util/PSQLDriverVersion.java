2f2c3de559d09eca0db2cbf9704264fd95efba50
jurka <jurka>
2011-09-11 01:39:32 +0000

    Open HEAD for 9.2 development.

--- org/postgresql/util/PSQLDriverVersion.java
+++ org/postgresql/util/PSQLDriverVersion.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2011, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/util/PSQLDriverVersion.java,v 1.31 2011/08/02 13:50:29 davecramer Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/util/PSQLDriverVersion.java,v 1.32 2011/09/11 01:39:32 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -21,7 +21,7 @@ import org.postgresql.Driver;
  */
 public class PSQLDriverVersion {
 
-    public final static int buildNumber = 900;
+    public final static int buildNumber = 1000;
 
     public static void main(String args[]) {
         java.net.URL url = Driver.class.getResource("/org/postgresql/Driver.class");
