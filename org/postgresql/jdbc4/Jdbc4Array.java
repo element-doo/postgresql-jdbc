14b109894c5eeb87b5cee8cbcbf92ef51e6bf5cf
davecramer <davecramer>
2011-09-26 12:52:29 +0000

    Mikko Tiihonen patches for binary types

--- org/postgresql/jdbc4/Jdbc4Array.java
+++ org/postgresql/jdbc4/Jdbc4Array.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2011, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/jdbc4/Jdbc4Array.java,v 1.5 2011/08/02 13:49:23 davecramer Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/jdbc4/Jdbc4Array.java,v 1.6 2011/09/26 12:52:31 davecramer Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -19,6 +19,11 @@ public class Jdbc4Array extends org.postgresql.jdbc2.AbstractJdbc2Array implemen
     public Jdbc4Array(BaseConnection conn, int oid, String fieldString) throws SQLException
     {
         super(conn, oid, fieldString);
+    }
+
+    public Jdbc4Array(BaseConnection conn, int oid, byte[] fieldBytes) throws SQLException
+    {
+        super(conn, oid, fieldBytes);
     }
 
     public Object getArray(Map < String, Class < ? >> map) throws SQLException
